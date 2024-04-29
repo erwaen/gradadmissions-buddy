639b392a3ce3cb5e6f92e8a8131a090718b7f7aa
Ctroya <carlostroya1@gmail.com>
2024-04-29 11:19:01 -0400

    JSON formatting

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 load_dotenv()
-
+import json
 app = FastAPI()
 
 
@@ -21,13 +21,13 @@ class QueryInput(BaseModel):
 class QueryOutput(BaseModel):
     response: str
 
-import json
+
 
 class DataSplitter:
     def __init__(self, input_file, output_file, chunk_size=500):
         self.input_file = input_file
         self.output_file = output_file
-        self.chunk_size = chunk_size  # Max characters per chunk
+        self.chunk_size = chunk_size
 
     def load_data(self):
         with open(self.input_file, 'r', encoding='utf-8') as file:
@@ -75,6 +75,61 @@ class DataSplitter:
                 processed_data.append(new_item)
 
         self.save_data(processed_data)
+
+
+class UniversityDataHandler:
+    def __init__(self, weaviate_url):
+        self.client = weaviate.Client(weaviate_url)
+        self.setup_schema()
+
+    def setup_schema(self):
+        schema = {
+            "classes": [
+                {
+                    "class": "University",
+                    "properties": [
+                        {"name": "url", "dataType": ["string"], "indexInverted": True},
+                        {"name": "university_name", "dataType": ["string"], "indexInverted": True},
+                    ]
+                },
+                {
+                    "class": "UniversityPage",
+                    "properties": [
+                        {"name": "content", "dataType": ["text"], "indexInverted": True},
+                        {"name": "university", "dataType": ["University"], "cardinality": "atMostOne"},
+                        {"name": "next_chunk", "dataType": ["UniversityPage"], "cardinality": "atMostOne"},
+                    ]
+                }
+            ]
+        }
+        self.client.schema.delete_all()
+        self.client.schema.create(schema)
+
+    def add_university(self, uuid, url, name):
+        """Add a university object to Weaviate."""
+        university_object = {
+            "url": url,
+            "university_name": name
+        }
+        self.client.data_object.create(university_object, "University", uuid)
+
+    def add_university_page(self, uuid, content, university_uuid, next_chunk_uuid=None):
+        """Add a university page object to Weaviate with optional reference to the next chunk."""
+        page_object = {
+            "content": content,
+            "university": {
+                "beacon": f"weaviate://localhost/University/{university_uuid}"
+            }
+        }
+        self.client.data_object.create(page_object, "UniversityPage", uuid)
+        if next_chunk_uuid:
+            self.client.data_object.add_reference(uuid, "UniversityPage", "next_chunk", next_chunk_uuid)
+
+
+# handler = UniversityDataHandler('http://localhost:8080')
+# handler.add_university("univ-harvard", "https://college.harvard.edu/admissions", "Harvard University")
+# handler.add_university_page("page1", "Page content 1", "univ-harvard", "page2")
+# handler.add_university_page("page2", "Page content 2", "univ-harvard")
 
 @app.post("/update-DB/")
 async def process_data():
