117de02ab1453c4ef83f3552b9971c8585acba1c
JuliaRecalde <yuliantonia@gmail.com>
2024-05-15 14:07:37 -0400

    add metodo para limpiar el texto extraido

--- univspider/univspider/spiders/uni_spider.py
+++ univspider/univspider/spiders/uni_spider.py
@@ -1,7 +1,11 @@
 import scrapy
 import json
 import os
+import datetime
 import re
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 class UniversitySpider(scrapy.Spider):
     name = 'university'
@@ -15,7 +19,16 @@ class UniversitySpider(scrapy.Spider):
         ("https://www.cs.rochester.edu/graduate/masters-program.html", "University of Rochester")
     ]
     max_depth_per_university = 2
+    visited_urls = set()
+    
+    def start_requests(self):
+        universities_arg = getattr(self, 'universities', None)
+        universities_to_scrape = universities_arg.split(',') if universities_arg else None
 
+        for idx, (url, university_name) in enumerate(self.start_urls, start=1):
+            if universities_to_scrape is None or str(idx) in universities_to_scrape:
+                yield scrapy.Request(url, meta={'id': idx, 'university_name': university_name, 'depth': 1}, callback=self.parse)
+    
     def parse(self, response):
         university_id = response.meta['id']
         university_name = response.meta['university_name']
@@ -23,13 +36,12 @@ class UniversitySpider(scrapy.Spider):
         title = response.css('h1::text').get()
         title = title.strip() if title else ''
         
-        content = response.xpath('//body//text()').getall()  # Obtener todo el texto dentro del cuerpo
-        content = ' '.join(content)  # Convertir la lista de strings a un solo string
-        content = re.sub(r'\s+', ' ', content)  # Eliminar espacios en blanco adicionales
+        content = self.fetch_text_from_url(response.url)  # Obtener solo el texto del cuerpo de la página
 
         if content:
             item = {
                 'id': university_id,
+                'data': datetime.datetime.now().isoformat(),
                 'url': response.url,
                 'university_name': university_name,
                 'title': title,
@@ -43,7 +55,33 @@ class UniversitySpider(scrapy.Spider):
             links = response.css('a::attr(href)').getall()
             for link in links:
                 if self.is_edu_link(link):
-                    yield response.follow(link, meta={'id': university_id, 'university_name': university_name, 'depth': depth + 1}, callback=self.parse)
+                    # Verificar si el enlace ya ha sido visitado antes de seguirlo
+                    next_link = response.urljoin(link)
+                    if next_link not in self.visited_urls:
+                        self.visited_urls.add(next_link)  # Agregar el enlace al conjunto de URLs visitadas
+                        yield response.follow(link, meta={'id': university_id, 'university_name': university_name, 'depth': depth + 1}, callback=self.parse)
+
+    def fetch_text_from_url(self, url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # Encuentra el cuerpo de la página y extrae el texto
+            body = soup.find('body')
+            if body:
+                return self.clean_text(body.get_text(separator=' ', strip=True))
+            else:
+                return None
+        except requests.RequestException as e:
+            print(f"Error fetching {url}: {e}")
+            return None
+    
+    def clean_text(self, text):
+        # Elimina las variables de plantilla y otros patrones no deseados
+        cleaned_text = re.sub(r'\{\{.*?\}\}', '', text)
+        cleaned_text = re.sub(r'\{\{.*?$', '', cleaned_text)  # Elimina líneas incompletas con patrón de plantilla
+        cleaned_text = re.sub(r'Add to GMail Close', '', cleaned_text)  # Elimina líneas específicas
+        return cleaned_text.strip()
 
     def save_item(self, item, university_id):
         filename = f'archivos_json/university_{university_id}.json'
@@ -54,10 +92,6 @@ class UniversitySpider(scrapy.Spider):
                 f.write(",\n")
             json.dump(item, f, ensure_ascii=False)
             f.write("\n")
-        
-    def start_requests(self):
-        for idx, (url, university_name) in enumerate(self.start_urls, start=1):
-            yield scrapy.Request(url, meta={'id': idx, 'university_name': university_name, 'depth': 1}, callback=self.parse)
 
     def closed(self, reason):
         for idx, _ in enumerate(self.start_urls, start=1):
